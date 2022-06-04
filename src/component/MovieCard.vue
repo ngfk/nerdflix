c965ff877b29e2d5dee3b23b3b33623a65012768
NGF Koster <ngf.koster@gmail.com>
2022-06-04 12:00:28 +0200

    fix: handle image not found

--- src/component/MovieCard.vue
+++ src/component/MovieCard.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="movie-card">
-    <img :src="movie.urlPoster" :alt="`Poster: ${movie.title}`" />
+    <div class="movie-card__poster">
+      <img
+        v-if="image"
+        :src="movie.urlPoster"
+        :alt="`Poster: ${movie.title}`"
+        @error="image = false"
+      />
+      <p v-else>Image not found</p>
+    </div>
 
     <p class="title">{{ movie.title }}</p>
     <p class="year">{{ movie.year }}</p>
@@ -8,6 +16,7 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { Movie } from '../model/movie';
 
 interface Props {
@@ -15,17 +24,38 @@ interface Props {
 }
 
 defineProps<Props>();
+
+const image = ref(true);
 </script>
 
 <style lang="scss" setup>
 .movie-card {
   display: flex;
   flex-flow: column nowrap;
-  max-width: 180px;
+  width: 100%;
 
-  img {
-    border-radius: 4px;
+  &__poster {
+    position: relative;
+    padding-top: min(calc((3 / 2) * 100%), 268px);
+    width: 100%;
+    max-width: 182px;
+    border-radius: 6px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    background-color: #131313;
+    overflow: hidden;
+
+    img,
+    p {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      text-align: center;
+    }
+
+    p {
+      opacity: 0.7;
+    }
   }
 
   .title {
--- src/component/MovieGrid.vue
+++ src/component/MovieGrid.vue
@@ -21,7 +21,6 @@ defineProps<Props>();
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
   grid-gap: 50px 30px;
-  align-items: flex-end;
   justify-items: center;
 }
 </style>

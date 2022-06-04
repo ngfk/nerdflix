b37ae13b5540b0adddf10a254c32de24625b3a6c
NGF Koster <ngf.koster@gmail.com>
2022-06-04 11:02:00 +0200

    feat: start action bar

---
+++ src/component/Input.vue
@@ -0,0 +1,36 @@
+<template>
+  <input
+    :placeholder="placeholder"
+    :type="type"
+    :value="modelValue"
+    @input="e => $emit('update:modelValue', (e.target as HTMLInputElement).value)"
+  />
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  modelValue: string;
+  placeholder: string;
+  type: string;
+}
+
+interface Emits {
+  (eventName: 'update:modelValue', value: string): void;
+}
+
+defineProps<Props>();
+defineEmits<Emits>();
+</script>
+
+<style lang="scss" scoped>
+input {
+  max-width: 394px;
+  width: 100%;
+  height: 48px;
+  background-color: #131313;
+  border: 1px solid #2e2e2e;
+  color: #fff;
+  border-radius: 6px;
+  padding: 13.5px 20px;
+}
+</style>
--- src/page/Movies.vue
+++ src/page/Movies.vue
@@ -1,7 +1,27 @@
 <template>
   <Title>Movies</Title>
+
+  <section class="action-bar">
+    <Input v-model="query" placeholder="Search for a title" type="search" />
+  </section>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+import Input from '../component/Input.vue';
 import Title from '../component/Title.vue';
+
+const query = ref('');
 </script>
+
+<style lang="scss" scoped>
+section {
+  margin: 0 108px;
+}
+
+.action-bar {
+  margin-top: -24px;
+  margin-bottom: 56px;
+}
+</style>
